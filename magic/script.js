bc64020203d50724f1cf82fe02745d39ecc1d062
keyur navadiya <iw0098>
2022-05-17 14:44:53 +0530

    magic

---
+++ magic/script.js
@@ -0,0 +1,36 @@
+const button = document.querySelectorAll("button");
+const box = document.querySelectorAll(".box");
+const rand = document.querySelectorAll(".rand");
+const answer = document.querySelector("#Answer");
+
+const randomNumber = Math.floor(Math.random() * 100);
+let boxWidth = box[0].clientWidth;
+
+const moveBox = () => {
+    box.forEach((item) => {
+        item.style.transform = "translateX(" + -boxWidth + "px) ";
+    });
+    boxWidth += box[0].clientWidth;
+};
+
+button.forEach((boxId, key) => {
+    boxId.addEventListener('click', () => {
+        if (boxId.id == "ansBox") {
+            this.addEventListener('click', function () {
+                this.window.location.href = "./";
+            });
+        }
+        moveBox();
+
+        button[key].parentNode.style.opacity = "0";
+        button[key + 1].parentNode.style.opacity = "1";
+    });
+});
+
+rand.forEach(num => {
+    num.innerHTML = randomNumber;
+});
+
+answer.innerHTML = randomNumber / 2;
+
+
