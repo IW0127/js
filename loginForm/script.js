54b18207dfde6786859ab1ee326f71545bb1f67a
keyur navadiya <iw0098>
2022-05-24 18:40:36 +0530

    val

--- loginForm/script.js
+++ loginForm/script.js
@@ -42,13 +42,70 @@ const validation = () => {
 
     const formData = new FormData(myForm);
     const username = formData.get("username");
+    const pNumber = formData.get('pNumber');
     const email = formData.get("email");
     const gender = formData.get("gender");
     const hobbies = formData.getAll("hobbies");
     const country = formData.get("country");
     const state = formData.get("state");
     const city = formData.get("city");
-    const all = { username, email, gender, hobbies, country, state, city };
+    const all = { username, pNumber, email, gender, hobbies, country, state, city };
     console.log(all);
 
+    if (username != "") {
+        const regExp = /^[a-zA-Z]+$/;
+        const user = regExp.test(username);
+        if (user == false) {
+            document.querySelector(".username").innerHTML = "space and Number Not allowed!";
+        } else {
+            document.querySelector(".username").innerHTML = "";
+        }
+    } else {
+        document.querySelector(".username").innerHTML = "Please fill Username.";
+
+    }
+
+    if (pNumber != "") {
+        const regExp = /^\+?([0-9]{2})?[ ]?([0-9]{10})$/
+        if (!pNumber.match(regExp)) {
+            document.querySelector(".pNumber").innerHTML = "Invalid Your Phone Number.";
+        } else {
+            document.querySelector(".pNumber").innerHTML = "";
+        }
+    } else {
+        document.querySelector(".pNumber").innerHTML = "Please fill Phone Number.";
+    }
+
+
+    if (email != "") {
+        const regExp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        if (!regExp.test(email)) {
+            document.querySelector(".email").innerHTML = "Invalid Email.";
+
+        } else {
+            document.querySelector(".email").innerHTML = "";
+
+        }
+    } else {
+        document.querySelector(".email").innerHTML = "Please fill Email.";
+
+    }
+
+    if ((gender != null)) {
+        document.querySelector(".gender").innerHTML = "";
+    } else {
+        document.querySelector(".gender").innerHTML = "Specify Your Gender.";
+        console.log("yes");
+    }
+
+    if (hobbies != "") {
+        document.querySelector(".hobbies").innerHTML = "";
+    } else {
+        document.querySelector(".hobbies").innerHTML = "Specify Your hobbies.";
+    }
+    if (hobbies != "") {
+        document.querySelector(".hobbies").innerHTML = "";
+    } else {
+        document.querySelector(".hobbies").innerHTML = "Specify Your hobbies.";
+    }
 }
