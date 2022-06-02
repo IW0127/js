86a0ac47c6b15d6a4bec2c5339633c426982631c
keyur navadiya <iw0098>
2022-06-02 15:52:36 +0530

    movie

--- Movie/script.js
+++ Movie/script.js
@@ -3,15 +3,37 @@ let Hours = document.getElementsByClassName('hour');
 let Minutes = document.getElementsByClassName('min');
 let Seconds = document.getElementsByClassName('sec');
 let BookNow = document.getElementById('bookNow');
+let massage = document.getElementsByClassName('massage');
+let error = document.getElementsByClassName('error');
+let timer = document.getElementsByClassName('timer');
+let d = 0;
 
-let day = 0;
-let hour = 0;
-let minute = 0;
-let second = 20;
+let T = JSON.parse(localStorage.time);
+let day;
+let hour;
+let minute;
+let second;
+
+if (T) {
+    day = T.days;
+    hour = T.hours;
+    minute = T.minutes;
+    second = T.seconds;
+}
 const timeOffer = () => {
     if ((day == 0) && (minute == 0) && (hour == 0) && (second == 0)) {
-        clearInterval(d);
-        #686363/*  */
+        timer[0].style.display = 'none';
+        BookNow.style.cssText = `
+        background-color:#686363;
+        cursor: not-allowed;`;
+        BookNow.disabled = true;
+        massage[0].style.display = 'none';
+        error[0].style.display = 'block';
+        d--;
+    }
+    if ((d < -1)) {
+        error[0].style.display = 'none';
+        clearInterval(tim);
     }
     if (day > 0) {
         Days[0].innerHTML = day;
@@ -22,6 +44,7 @@ const timeOffer = () => {
         hour = 24;
         Hours[0].innerHTML = hour;
     }
+
     if (hour > 0) {
         Hours[0].innerHTML = hour;
     }
@@ -31,6 +54,7 @@ const timeOffer = () => {
         minute = 59;
         Minutes[0].innerHTML = minute;
     }
+
     if (second == 0 && minute != 0) {
         minute--;
         Minutes[0].innerHTML = minute;
@@ -42,5 +66,4 @@ const timeOffer = () => {
     }
 }
 
-const d = setInterval(timeOffer, 1000);
-console.log(Days);
+const tim = setInterval(timeOffer, 1000);
